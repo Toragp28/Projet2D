8e9aac2744591ece659bf85ca1ac1bac453027b6
Flixyop <108677630+Flixyop@users.noreply.github.com>
2024-10-16 19:27:14 +0200

    player_attak_anim

--- player.gd
+++ player.gd
@@ -3,6 +3,14 @@ extends CharacterBody2D
 
 ##################################### VARIABLE #############################################
 
+enum STATE {
+	Idle,
+	Move,
+	Attack
+	}
+
+var state = STATE.Idle
+
 #Variable Paramètre
 @export var speed = 400
 
@@ -10,29 +18,68 @@ extends CharacterBody2D
 
 #Variable
 var input_direction = Vector2(0, 0)
+var dir = Vector2.ZERO
+
 
 ################################### FONCTION ##################################################
 
+func _ready() -> void:
+	$AnimatedSprite2D.play("idle_down")
 
 func get_input():
 	input_direction = Input.get_vector("left", "right", "up", "down")
+	
+	
+
+func animatation():
 	velocity = input_direction * speed
 	
 	if input_direction == Vector2(1, 0):
+		dir = input_direction
 		$AnimatedSprite2D.play("move_right")
 		
 	elif input_direction == Vector2(-1, 0):
+		dir = input_direction
 		$AnimatedSprite2D.play("move_left")
 		
 	elif input_direction == Vector2(0, 1):
+		dir = input_direction
 		$AnimatedSprite2D.play("move_down")
 		
 	elif input_direction == Vector2(0, -1):
+		dir = input_direction
 		$AnimatedSprite2D.play("move_up")
+		
+	if Input.is_action_just_pressed("sword_attack"):
+
+		if dir == Vector2(0, 1):
+			$AnimatedSprite2D.play("sword_attack_down")
+		if dir == Vector2(0, -1):
+			$AnimatedSprite2D.play("sword_attack_up")
+		if dir == Vector2(1, 0):
+			$AnimatedSprite2D.play("sword_attack_right")
+		if dir == Vector2(-1, 0):
+			$AnimatedSprite2D.play("sword_attack_left")
+			
 
 		
 	
 	
 func _physics_process(delta):
+	
 	get_input()
+	animatation()
 	move_and_slide()
+	
+	
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if $AnimatedSprite2D.animation == "sword_attack_down":
+		$AnimatedSprite2D.play("idle_down")
+	if $AnimatedSprite2D.animation == "sword_attack_up":
+		$AnimatedSprite2D.play("idle_up")
+	if $AnimatedSprite2D.animation == "sword_attack_right":
+		$AnimatedSprite2D.play("idle_right")
+	if $AnimatedSprite2D.animation == "sword_attack_left":
+		$AnimatedSprite2D.play("idle_left")
